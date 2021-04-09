b700f643bdaf4a27a6e5668e53be3ce0b85b9ac5
Dylan <dylanbrookschi@gmail.com>
2021-04-09 12:06:11 -0500

    phase 1 complete

--- tree.py
+++ tree.py
@@ -42,16 +42,60 @@ class Node:
     if parent is not None:
       parent.add_child(self)
 
+  def breadth_search(self, value):
+    queue = [self]
+    while queue:
+      node = queue.pop(0)
+      if node._value == value:
+        return node
+      else:
+        for child in node._children:
+          queue.append(child)
+
+        # can extend without loop
+
+  def depth_search(self, value):
+    if self._value == value:
+      return self
+    for child in self._children:
+      node = child.depth_search(value)
+      if node is not None:
+        return node
+
+
+    # def depth_search(self, value):
+    #   if self._value == value:
+    #     return self
+    #   for child in self._children:
+    #     if child is not None:
+    #       return child.depth_search(value)
+
+  
+  
+  # def depth_search(self, value):
+  #   if self._value == value:
+  #     return self
+  #   for child in self._children:
+  #     return child.depth_search(value)
+    
 
 
 
 
-node1 = Node("root1")
-node2 = Node("root2")
-node3 = Node("root3")
 
-node3.parent = node1
-node3.parent = node2
 
-print(node1.children)
-print(node2.children)
+
+# node1 = Node("root1")
+# node2 = Node("root2")
+# node3 = Node("root3")
+# node4 = Node("root4")
+# node5 = Node("root5")
+# node6 = Node("root6")
+
+# node3.parent = node1
+# node2.parent = node1
+# node4.parent = node1
+# node5.parent = node1
+# node6.parent = node1
+
+# print(node1.depth_search('bogus'))
